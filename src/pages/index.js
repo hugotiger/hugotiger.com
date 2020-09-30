7e5762dacc2c84e5133cdff8083dc66946e0a5c2
Hugo Holm <hugo.tiger.holm@gmail.com>
2020-10-01 01:37:56 +0200

    Added title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ import { Helmet } from "react-helmet"
 const IndexPage = () => (
   <>
     <Helmet>
+      <title>Hugo Tiger</title>
       <link
         href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap"
         rel="stylesheet"
