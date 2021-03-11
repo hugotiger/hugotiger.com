02ef6826103acf99410e0a146222235e0d52183e
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-03-11 15:08:35 +0100

    Rearrange imports

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useLayoutEffect, useState } from "react"
-import styled from "styled-components"
 import { motion } from "framer-motion"
+import React from "react"
+import styled from "styled-components"
 
 const SectionWrapper = styled(motion.section)`
   margin-top: 4rem;
